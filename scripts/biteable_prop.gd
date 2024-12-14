45ad5f2a18ed086ab357e6bc5e8f6ba3a8bac57d
kennethbchen <kennethbchen@gmail.com>
2024-12-14 12:33:01 -0500

    Implemented inheriting positional and angular velocity when a bitten object is released

--- scripts/biteable_prop.gd
+++ scripts/biteable_prop.gd
@@ -4,16 +4,37 @@ enum STATE {NORMAL, BITTEN, DESTROYED}
 
 var current_state: STATE = STATE.NORMAL
 
+var prev_rotation: float
+var prev_position: Vector2
+
+var angular_velocity: float = 0.0
+
+var estimated_velocity: Vector2
+var estimated_angular_velocity: float
+
 func _ready() -> void:
-	velocity = Vector2(0, -10)
+	pass
 
 func _process(delta: float) -> void:
 	
 	match current_state:
 		STATE.NORMAL:
+			
+			rotation += angular_velocity * delta
+			
 			move_and_slide()
+			
 		STATE.BITTEN:
-			# Don't do anything and just be controlled by biter
+			# Don't move and just be controlled by biter
+			
+			# Estimate velocity based on diff between prev and current position
+			estimated_velocity = (position - prev_position) / delta
+			prev_position = global_position
+			
+			# Estimate angular velocity
+			estimated_angular_velocity = (rotation - prev_rotation) / delta
+			prev_rotation = global_rotation
+			
 			return
 	
 func _on_bite_started() -> void:
@@ -22,3 +43,6 @@ func _on_bite_started() -> void:
 	
 func _on_bite_ended() -> void:
 	current_state = STATE.NORMAL
+	
+	velocity = estimated_velocity
+	angular_velocity = estimated_angular_velocity
